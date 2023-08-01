8e614e104378c4113af1da716e1c0cfbaa365b65
SquareFace <sq8.square.face@gmail.com>
2023-08-01 14:30:55 +0100

    disabled dynamic colors

--- app/src/main/java/com/example/schoolhard/ui/theme/Theme.kt
+++ app/src/main/java/com/example/schoolhard/ui/theme/Theme.kt
@@ -47,7 +47,7 @@ private val LightColorScheme = lightColorScheme(
 fun SchoolHardTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
