4faa7f237eaf2af475e54d13f5ad930c6332043e
SquareFace <sq8.square.face@gmail.com>
2023-08-02 23:18:37 +0100

    first version of the Top Menu for accessing the nav drawer

--- app/src/main/java/com/example/schoolhard/ui/SchooHardApp.kt
+++ app/src/main/java/com/example/schoolhard/ui/SchooHardApp.kt
@@ -6,8 +6,10 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.add
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -63,7 +65,7 @@ fun SchoolHardApp(
             gesturesEnabled = !isExpandedScreen,
             modifier = Modifier.background(MaterialTheme.colorScheme.background)
         ) {
-            Row {
+            Row(Modifier.systemBarsPadding().fillMaxSize()) {
                 if (isExpandedScreen) {
                     AppNavRail(
                         currentRoute = currentRoute,
--- app/src/main/java/com/example/schoolhard/ui/SchoolHardNavGraph.kt
+++ app/src/main/java/com/example/schoolhard/ui/SchoolHardNavGraph.kt
@@ -1,11 +1,27 @@
 package com.example.schoolhard.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -22,25 +38,48 @@ fun SchoolHardNavGraph(
     openDrawer: () -> Unit = {},
     startDestination: String = SchoolHardDestinations.HOME_ROUTE,
 ) {
-    SchoolHardLogo(modifier = Modifier.clickable { openDrawer() })
-    NavHost(
-        navController = navController,
-        startDestination = startDestination,
-        modifier = modifier
+    Column(modifier
+
+
     ) {
-        composable(
-            route = SchoolHardDestinations.HOME_ROUTE,
-            deepLinks = listOf(
-                navDeepLink {
-                    uriPattern =
-                        "$SchoolHard_APP_URI/${SchoolHardDestinations.HOME_ROUTE}?$POST_ID={$POST_ID}"
-                }
+        Row(
+            Modifier
+                .background(MaterialTheme.colorScheme.primary)
+                .fillMaxWidth()
+                .padding(7.dp)
+                .clickable { openDrawer() },
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(Icons.Filled.Menu, null, tint = Color.White, modifier = Modifier.scale(1.5F).padding(15.dp, 10.dp, 0.dp, 10.dp))
+            Spacer(modifier = Modifier.width(30.dp))
+            Text(
+                text = "SchoolHard",
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight(600),
+                    color = Color(0xFFFFFFFF),
+                )
             )
-        ) { navBackStackEntry ->
-            Text(modifier = Modifier.padding(30.dp), text="Home")
         }
-        composable(SchoolHardDestinations.Today_ROUTE) {
-            Text(modifier = Modifier.padding(30.dp), text="Today")
+        NavHost(
+            navController = navController,
+            startDestination = startDestination,
+            modifier = modifier
+        ) {
+            composable(
+                route = SchoolHardDestinations.HOME_ROUTE,
+                deepLinks = listOf(
+                    navDeepLink {
+                        uriPattern =
+                            "$SchoolHard_APP_URI/${SchoolHardDestinations.HOME_ROUTE}?$POST_ID={$POST_ID}"
+                    }
+                )
+            ) { navBackStackEntry ->
+                Text(modifier = Modifier.padding(30.dp), text="Home")
+            }
+            composable(SchoolHardDestinations.Today_ROUTE) {
+                Text(modifier = Modifier.padding(30.dp), text="Today")
+            }
         }
     }
 }
