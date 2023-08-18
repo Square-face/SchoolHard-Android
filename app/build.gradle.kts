a83c9e5901be5205f2dcd02a9b0fb885b35a33ae
SquareFace <sq8.square.face@gmail.com>
2023-08-18 19:30:34 +0200

    sdk bump to allow the use of LocalDateTime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "square.face.schoolhard"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
