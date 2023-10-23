1b9d8c92147f9b38f863ca068f8a7c8178e3302f
Square-Face <sq8.square.face@gmail.com>
2023-10-23 15:57:09 +0200

    Added further database functionality to aid in the creation of the home page

--- app/src/main/java/com/example/schoolhard/database/Utils.kt
+++ app/src/main/java/com/example/schoolhard/database/Utils.kt
@@ -7,6 +7,7 @@ import com.example.schoolhard.API.Occasion
 import com.example.schoolhard.API.Subject
 import java.time.DayOfWeek
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.LocalTime
 import java.util.UUID
 
@@ -14,70 +15,135 @@ import java.util.UUID
 /**
  * Helper class with utility functions used by [Database]
  * */
-class Utils {
+class Utils(private val database: Database) {
 
 
 
 
 
     /**
-     * Parse a lesson from a cursor and the position it is currently in
+     * Parses a lesson from a cursor and the position it is currently in.
      *
-     * @param db Database to use to get occasions or subjects if they don't exist
-     * in [occasions] or [subjects]
-     * @param cursor Cursor to parse from
-     * @param occasions optional list of already parsed occasions
-     * @param subjects optional list of already parsed subjects
-     * */
+     * @param cursor Cursor to parse from.
+     * @param occasions Optional list of already parsed occasions.
+     * @param subjects Optional list of already parsed subjects.
+     * @return The parsed Lesson object.
+     */
     fun parseLesson(
-        db: Database,
         cursor: Cursor,
         occasions: MutableList<Occasion> = mutableListOf(),
         subjects: MutableList<Subject> = mutableListOf(),
     ): Lesson {
+        val columns = Schema.Lesson.Columns // Define a variable for convenience
 
-        val occasionUUID = cursor.getString(Schema.Lesson.Columns.occasionUUID.index)
-        val subjectUUID = cursor.getString(Schema.Lesson.Columns.subjectUUID.index)
+        // Extract occasion and subject UUIDs from the cursor
+        val occasionUUID = cursor.getString(columns.occasionUUID.index)
+        val subjectUUID = cursor.getString(columns.subjectUUID.index)
 
-        val subject = findOrGet(db, subjects, subjectUUID)
-        val occasion = findOrGet(db, subject, occasions, occasionUUID)
+        // Find or retrieve the subject and occasion based on their UUIDs
+        val subject = findOrGet(subjects, subjectUUID)
+        val occasion = findOrGet(subject, occasions, occasionUUID)
 
+        // Calculate the date using days since 1970-01-01
+        val date = calculateDateFromCursor(cursor, columns.date.index)
+
+        // Create and return the Lesson object
         return Lesson(
             occasion,
-            cursor.getInt(Schema.Lesson.Columns.week.index),
-            LocalDate.ofEpochDay(cursor.getInt(Schema.Lesson.Columns.date.index).toLong()),
-            UUID.fromString(cursor.getString(Schema.Lesson.Columns.uuid.index))
+            cursor.getInt(columns.week.index),
+            LocalDate.ofEpochDay(cursor.getInt(columns.date.index).toLong()),
+            UUID.fromString(cursor.getString(columns.uuid.index))
         )
+    }
+
+    /**
+     * Calculates the date from a cursor based on the number of days since 1970-01-01.
+     *
+     * @param cursor The cursor containing the date information.
+     * @param columnIndex The index of the column containing the number of days.
+     * @return The calculated LocalDate.
+     */
+    private fun calculateDateFromCursor(cursor: Cursor, columnIndex: Int): LocalDate {
+        val daysSince1970 = cursor.getInt(columnIndex).toLong()
+        return LocalDate.ofEpochDay(daysSince1970)
+    }
+
+
+
+
+
+    /**
+     * Parses an Occasion from a database cursor, subject list, and other data.
+     *
+     * @param cursor The cursor containing the occasion data.
+     * @param subjects The list of subjects used to associate with the occasion.
+     * @return The parsed Occasion object.
+     */
+    fun parseOccasion(
+        cursor: Cursor,
+        subjects: MutableList<Subject>
+    ): Occasion {
+        // column quick access
+        val columns = Schema.Occasion.Columns
+
+        // Extract the subject UUID from the cursor
+        val subjectUUID     = cursor.getString(columns.subjectUUID.index)
+
+        // Find or retrieve the subject based on the subject UUID
+        val subject         = findSubjectByUUID(subjects, subjectUUID)
+
+        // Extract various data from the cursor to create an Occasion
+        val occasionUUID    = UUID.fromString(cursor.getString(columns.uuid.index))
+        val occasionId      = cursor.getInt(columns.occasionId.index)
+        val location        = Location(cursor.getString(columns.location.index))
+        val startTime       = extractLocalTime(cursor, columns.startTime.index)
+        val endTime         = extractLocalTime(cursor, columns.endTime.index)
+        val dayOfWeek       = extractDayOfWeek(cursor, columns.dayOfWeek.index)
+
+        // Create and return the Occasion object
+        return Occasion(occasionUUID, occasionId, subject, location, startTime, endTime, dayOfWeek)
     }
 
 
     /**
-     * Parse an occasion from a cursor and the position it is currently in
+     * Finds a subject in the provided list of subjects based on its UUID. If the subject
+     * with the specified UUID is not found in the list, it is retrieved from the database
+     * and added to the list.
      *
-     * @param db Database to use to get occasions or subjects if they don't exist
-     * in [subjects]
-     * @param cursor Cursor to parse from
-     * @param subjects optional list of already parsed subjects
-     * */
-    fun parseOccasion(
-        db: Database,
-        cursor: Cursor,
-        subjects: MutableList<Subject>
-    ): Occasion {
-
-        val subjectUUID = cursor.getString(Schema.Occasion.Columns.subjectUUID.index)
-        val subject = findOrGet(db, subjects, subjectUUID)
-
-        return Occasion(
-            UUID.fromString(cursor.getString(Schema.Occasion.Columns.uuid.index)),
-            cursor.getInt(Schema.Occasion.Columns.occasionId.index),
-            subject,
-            Location(cursor.getString(Schema.Occasion.Columns.location.index)),
-            LocalTime.MIN.plusSeconds(cursor.getInt(Schema.Occasion.Columns.startTime.index).toLong()),
-            LocalTime.MIN.plusSeconds(cursor.getInt(Schema.Occasion.Columns.endTime.index).toLong()),
-            DayOfWeek.of(cursor.getInt(Schema.Occasion.Columns.dayOfWeek.index))
-        )
+     * @param subjects The list of subjects to search in or update.
+     * @param subjectUUID The UUID of the subject to find or retrieve.
+     * @return The subject with the specified UUID, either found in the list or retrieved from the database.
+     */
+    private fun findSubjectByUUID(subjects: MutableList<Subject>, subjectUUID: String): Subject {
+        return findOrGet(subjects, subjectUUID)
     }
+    /**
+     * Extracts a [LocalTime] value from the specified [Cursor] at the given column index.
+     *
+     * @param cursor The cursor to extract the value from.
+     * @param columnIndex The index of the column containing the LocalTime value.
+     * @return The extracted LocalTime.
+     */
+    private fun extractLocalTime(cursor: Cursor, columnIndex: Int): LocalTime {
+        return LocalTime.ofSecondOfDay(cursor.getInt(columnIndex).toLong())
+    }
+
+
+
+
+    /**
+     * Extracts a [DayOfWeek] value from the specified [Cursor] at the given column index.
+     *
+     * @param cursor The cursor to extract the value from.
+     * @param columnIndex The index of the column containing the DayOfWeek value.
+     * @return The extracted DayOfWeek.
+     */
+    private fun extractDayOfWeek(cursor: Cursor, columnIndex: Int): DayOfWeek {
+        return DayOfWeek.of(cursor.getInt(columnIndex))
+    }
+
+
+
 
 
     /**
@@ -103,20 +169,18 @@ class Utils {
      * If not found, get from the database and add it to [subjects].
      *
      *
-     * @param db Database to use if the subject isn't found
      * @param subjects List of previously cached subjects
      * @param uuid UUID string to search for
      *
      * @return Subject, generated or from the list
      * */
     private fun findOrGet(
-        db: Database,
         subjects: MutableList<Subject>,
         uuid: String,
     ): Subject {
 
         return subjects.find { it.id.toString() == uuid } ?: run{
-            val subject = db.getSubject(uuid)!!
+            val subject = database.getSubject(uuid)!!
             subjects.add(subject)
 
             return@run subject
@@ -130,7 +194,6 @@ class Utils {
      *
      * If not found, use [subject] as the parent and add it to [occasions].
      *
-     * @param db Database to use if the occasion isn't found
      * @param subject Parent to use if the occasion isn't found
      * @param occasions List of previously cached occasions
      * @param uuid UUID string to search for
@@ -138,14 +201,13 @@ class Utils {
      * @return Occasion generated or from the list
      * */
     private fun findOrGet(
-        db: Database,
         subject: Subject,
         occasions: MutableList<Occasion>,
         uuid: String,
     ): Occasion {
 
         return occasions.find { it.id.toString() == uuid } ?: run {
-            val occasion = db.getOccasion(uuid, subject)!!
+            val occasion = database.getOccasion(uuid, subject)!!
             occasions.add(occasion)
 
             return@run occasion
@@ -194,6 +256,74 @@ class Utils {
             }
 
 
+
+
+
+            /**
+             * Generate a query object with sql string and args
+             *
+             * @param after Optional start date
+             * @param before Optional end date
+             *
+             * @return Query object
+             * */
+            fun lessonQuery(after: LocalDateTime?=null, before: LocalDateTime?=null): Query {
+                var query = "SELECT * FROM ${Schema.Lesson.table}"
+                val args = mutableListOf<String>()
+
+                // only add WHERE if any args are not null
+                if (after != null || before != null) {
+                    query += " WHERE"
+                }
+
+                if (after != null) {
+                    // unknowns: date, startTime, date again
+
+                    query += " (${Schema.Lesson.Columns.date} = ? AND ${Schema.Lesson.Columns.startTime} >= ?) OR ${Schema.Lesson.Columns.date} > ?"
+                    args.add(after.toLocalDate().toEpochDay().toString())
+                    args.add(after.toLocalTime().toSecondOfDay().toString())
+                    args.add(after.toLocalDate().toEpochDay().toString())
+                }
+
+                if (before != null) {
+                    // insert AND if there exists any previous checks
+                    if (after != null) { query += " AND" }
+
+                    // unknowns: date, endTime
+                    query += " ${Schema.Lesson.Columns.date} <= ? AND ${Schema.Lesson.Columns.endTime} <= ?"
+                    args.add(before.toLocalDate().toEpochDay().toString())
+                    args.add(before.toLocalTime().toSecondOfDay().toString())
+                }
+
+                return Query(query, args.toTypedArray())
+            }
+
+
+
+
+            /**
+             * Get a query object for retrieving a lesson at a specific date
+             *
+             * @param at DateTime to get lesson at
+             * */
+            fun lessonQuery(at: LocalDateTime): Query {
+
+                // unknowns: date, startTime, endTime
+                val query = "SELECT * FROM ${Schema.Lesson.table} WHERE ${Schema.Lesson.Columns.date} = ? AND ${Schema.Lesson.Columns.startTime} <= ? AND ${Schema.Lesson.Columns.endTime} >= ?"
+
+                val args = arrayOf(
+                    at.toLocalDate().toEpochDay().toString(),
+                    at.toLocalTime().toSecondOfDay().toString(),
+                    at.toLocalTime().toSecondOfDay().toString()
+                )
+
+                return Query(query, args)
+            }
+
+
+
+
+
             /**
              * Generate a query object with sql string and args
              *
@@ -207,6 +337,8 @@ class Utils {
 
                 return Query(query, args)
             }
+
+
 
 
 
