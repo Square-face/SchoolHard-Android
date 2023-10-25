3fa000b862d772933423ef742cadf8ba73c2dcd1
Square-Face <sq8.square.face@gmail.com>
2023-10-25 13:18:55 +0200

    Added spacing in nav drawer

--- app/src/main/java/com/example/schoolhard/ui/NavDrawer.kt
+++ app/src/main/java/com/example/schoolhard/ui/NavDrawer.kt
@@ -4,12 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -73,6 +73,7 @@ fun AppDrawer(
             onClick = { navigateToSettings(); closeDrawer() },
             modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
         )
+        Spacer(modifier = Modifier.height(15.dp))
     }
 }
 
